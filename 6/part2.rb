7007b736df24a66df0f542679c4c5af2f9dd0bb4
Maeve Revels <maeve@aha.io>
2022-12-10 22:41:09 -0800

    Day 6 Part 2

--- 6/part2.rb
+++ 6/part2.rb
@@ -4,10 +4,10 @@ input = File.readlines('./input.txt').map(&:chomp)
 
 input.each do |line|
   line.chars.each_index do |i|
-    segment = line.chars.slice(i, 4)
+    segment = line.chars.slice(i, 14)
 
     if segment == segment.uniq
-      puts "Characters: #{i + 4}"
+      puts "Characters: #{i + 14}"
       break
     end
   end
