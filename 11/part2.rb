6fbc1e441739c1045a0f297b0abb81dc65f5b23a
Maeve Revels <maeve@aha.io>
2022-12-13 21:13:48 -0800

    Simplified Day 11 Part 2

--- 11/part2.rb
+++ 11/part2.rb
@@ -1,136 +1,42 @@
 #!/usr/bin/env ruby
 
-input = File.readlines('./test-input.txt').map(&:chomp)
+require 'prime'
 
-class Item
-  attr_accessor :divisor,
-                :quotient,
-                :remainder,
-                :id
-
-  def self.id_generator
-    @id_generator ||= 0
-    @id_generator += 1
-  end
-
-  def initialize(value)
-    self.id = Item.id_generator
-    self.divisor = 2
-
-    div, mod = divmod(value)
-    self.quotient = div
-    self.remainder = mod
-  end
-
-  def perform(operation, operand)
-    value = operand == :old ? clone : operand
-    send(operation, value)
-  end
-
-  def test(value)
-    mod = quotient % value
-    mod *= divisor % value
-    mod %= value
-
-    mod += (remainder % value)
-    mod %= value
-
-    mod.zero?
-  end
-
-  def to_s
-    "(id=#{id}): #{quotient} * #{divisor} + #{remainder} = #{to_i}"
-  end
-
-  def to_i
-    quotient * divisor + remainder
-  end
-
-  def *(other)
-    if other.is_a?(Item)
-      self.quotient *= other.divisor
-      self.divisor *= other.quotient
-      self.divisor += remainder + other.remainder
-      self.remainder *= other.remainder
-    else
-      self.divisor *= other
-      self.remainder *= other
-    end
-
-    rebalance
-    self
-  end
-
-  def +(other)
-    if other.is_a?(Item)
-      self.divisor += other.divisor
-      self.remainder += other.remainder
-    else
-      div, mod = divmod(other)
-      self.quotient += div
-      self.remainder += mod
-    end
-
-    rebalance
-    self
-  end
-
-  def -(other)
-    if other.is_a?(Item)
-      self.divisor -= other.divisor
-      self.remainder -= other.remainder
-    else
-      div, mod = divmod(other)
-      self.quotient -= div
-      self.remainder -= mod
-    end
-
-    rebalance
-    self
-  end
-
-  protected
-
-  def divmod(value)
-    value.divmod(divisor)
-  end
-
-  def rebalance
-    div, mod = divmod(remainder)
-    self.quotient += div
-    self.remainder = mod
-  end
-end
+input = File.readlines('./input.txt').map(&:chomp)
 
 class Monkey
-  attr_reader :name,
-              :items,
-              :operation,
+  attr_reader :name, 
+              :items, 
+              :operation, 
               :operand,
               :test,
               :outcomes,
-              :inspections
+              :inspections,
+              :product
 
-  def initialize(input)
-    lines = input.clone
+  def initialize(lines)
+    self.name = lines[0]
+    self.items = lines[1]
+    self.operation = lines[2]
+    self.test = lines[3]
 
-    self.name = lines.shift
-    self.items = lines.shift
-    self.operation = lines.shift
-    self.test = lines.shift
-
-    @outcomes = {}
-    add_outcome(lines.shift) until lines.empty?
+    @outcomes = {
+      true => target_monkey(split_line(lines[4]).last),
+      false => target_monkey(split_line(lines[5]).last),
+    }
 
     @inspections = 0
+
+    @product = Prime.first(9).inject(:*)
   end
 
-  def inspect_item
+  def inspect
     item = items.shift
 
-    item.perform(operation, operand)
+    item = item.send(operation, operand_for(item))
+    item = item % product
 
-    target = outcomes[item.test(test)]
+    target = outcomes[item % test == 0]
 
     @inspections += 1
 
@@ -144,31 +50,37 @@ class Monkey
   private
 
   def name=(line)
-    @name = line.strip.match(/Monkey ([0-9]+):/)[1]
+    @name = split_line(line).first.match(/Monkey ([0-9]+)/)[1]
   end
 
   def items=(line)
-    @items = line.strip.match(/Starting items: ([0-9, ]+)/)[1].split(',').map do |item|
-      Item.new(item.strip.to_i)
-    end
+    @items = split_line(line).last.split(',').map(&:to_i)
   end
 
   def operation=(line)
-    match = line.strip.match(/Operation: new = old (.) ([[0-9a-z]]+)/)
+    match = split_line(line).last.match(/new = old (.) ([[0-9a-z]]+)/)
     @operation = match[1].to_sym
     @operand = match[2] == 'old' ? :old : match[2].to_i
   end
 
   def test=(line)
-    @test = line.strip.match(/Test: divisible by ([0-9]+)/)[1].to_i
+    @test = split_line(line).last.match(/divisible by ([0-9]+)/)[1].to_i
   end
 
-  def add_outcome(line)
-    match = line.strip.match(/If ([a-z]+): throw to monkey ([0-9]+)/)
-    return unless match
+  def split_line(line)
+    line.split(':').map(&:strip)
+  end
 
-    outcome = (match[1] == 'true')
-    @outcomes[outcome] = match[2].to_i
+  def target_monkey(string)
+    string.match(/throw to monkey ([0-9]+)/)[1].to_i
+  end
+
+  def operand_for(value)
+    if operand == :old
+      value
+    else
+      operand.to_i
+    end
   end
 end
 
@@ -178,38 +90,19 @@ input.each_slice(7) do |lines|
   monkeys << Monkey.new(lines)
 end
 
-def print_monkeys(monkeys)
-  monkeys.each do |monkey|
-    puts "Monkey #{monkey.name} items:"
-    puts monkey.items
-  end
-
-  puts ''
-end
-
-# puts "== Initial state =="
-#
-# print_monkeys(monkeys)
-
-1000.times do |i|
+10000.times do |i|
   monkeys.each do |monkey|
     until monkey.items.empty?
-      target, item = monkey.inspect_item
+      target, item = monkey.inspect
       monkeys[target].items << item
     end
-
-    # puts "After Monkey #{monkey.name}'s turn:"
-    # print_monkeys(monkeys)
-  end
+ end
 
   puts "== After round #{i + 1} =="
+end
 
-  # print_monkeys(monkeys)
-
-  monkeys.each do |monkey|
-    puts "Monkey #{monkey.name} inspected items #{monkey.inspections} times."
-  end
-  puts ''
+monkeys.each do |monkey|
+  puts "Monkey #{monkey.name} inspected items #{monkey.inspections} times."
 end
 
 inspections = monkeys.map(&:inspections).sort.reverse
