43eb719598424f97dcf17eb9890f13491a372e36
Maeve Revels <maeve@aha.io>
2022-12-14 20:48:08 -0800

    Day 13 Part 1

--- 13/part1.rb
+++ 13/part1.rb
@@ -2,48 +2,59 @@
 
 require 'json'
 
-def correct_order?(value, other_value, depth=0)
-  indent = "  " * depth
+class Packet
+  include Comparable
 
-  left_arr = Array(value)
-  right_arr = Array(other_value)
+  attr_accessor :value
 
-  left_arr << nil if left_arr.size < right_arr.size
+  def initialize(value)
+    self.value = value
+  end
 
-  left_arr.zip(right_arr).each do |left, right|
-    puts "#{indent}- Compare #{left} vs #{right}"
-    next if left == right
-
-    if left.nil?
-      puts "#{indent}  - Left side ran out of items, so inputs are in the right order"
-      return true
-    elsif right.nil?
-      puts "#{indent}  - Right side ran out of items, so inputs are NOT in the right order"
-      return false
-    elsif left.is_a?(Array) || right.is_a?(Array)
-      return correct_order?(left, right, depth + 1)
-    elsif left < right
-      puts "#{indent}  - Left side is smaller, so inputs are in the right order"
-      return true
-    elsif left > right
-      puts "#{indent}  - Right side is smaller, so inputs are NOT in the right order"
-      return false
+  def <=>(other)
+    if value.nil? && other.value.nil?
+      0
+    elsif value.nil?
+      -1
+    elsif other.value.nil?
+      1
+    elsif value.is_a?(Integer) && other.value.is_a?(Integer)
+      value <=> other.value
+    else
+      compare_lists(Array(value), Array(other.value))
     end
+  end
+
+  protected
+
+  def compare_lists(list, other_list)
+    list << nil if list.length < other_list.length
+
+    result = 0
+
+    list.zip(other_list).each do |left, right|
+      result = Packet.new(left) <=> Packet.new(right)
+      break unless result.zero?
+    end
+
+    result
   end
 end
 
 input = File.readlines('./input.txt').map(&:chomp)
 
-index = 0
+pair_index = 0
 sum = 0
 
 input.each_slice(3) do |pair|
+  pair_index += 1
   left = JSON.parse(pair[0])
   right = JSON.parse(pair[1])
 
-  puts "== Pair #{index += 1} =="
-  puts "- Compare #{left.to_json} vs #{right.to_json}"
-  sum += index if correct_order?(left, right)
+  if Packet.new(left) < Packet.new(right)
+    puts "Adding pair #{pair_index} to sum"
+    sum += pair_index
+  end
 end
 
 puts "Sum: #{sum}"
