b92404d09bedde9ca78daf73ae3f304c59f9a2ce
Maeve Revels <maeve@aha.io>
2022-12-18 21:16:35 -0800

    Start down the path (ha!) of using Floyd-Warshall
    But it is possible that this will be too slow

--- 16/part1.rb
+++ 16/part1.rb
@@ -1,113 +1,109 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'matrix'
+
 class Valve
-  attr_accessor :name, :flow_rate, :tunnels
+  attr_accessor :id, :name, :flow_rate, :tunnels
+
+  class << self
+    def generate_id
+      @next_id ||= -1
+      @next_id += 1
+    end
+  end
 
   def initialize(name)
+    self.id = self.class.generate_id
     self.name = name
   end
 
   def to_s
-    "Valve #{name} has flow rate=#{flow_rate}; tunnels lead to #{tunnels.map(&:name).join(', ')}"
-  end
-end
-
-class ValvePath
-  attr_accessor :valve, :pressure, :opened, :visited
-
-  def initialize(valve, pressure, opened = [], visited = [])
-    self.valve = valve
-    self.pressure = pressure
-    self.opened = opened
-    self.visited = visited
-
-    # No-flow valves are the same open or closed, so consider them open
-    visit!
-    open! if valve.flow_rate.zero? && !open?
+    "Valve: id=#{id} name=#{name} flow_rate=#{flow_rate} tunnels=[#{tunnels.map(&:name).join(', ')}]"
   end
 
-  def update_pressure
-    self.pressure += opened.map(&:flow_rate).inject(:+).to_i
+  def ==(other)
+    name == other.name
   end
 
-  def open?
-    opened.include?(valve)
+  def eql?(other)
+    self == other
   end
 
-  def visit!
-    visited << valve
-    self
+  def hash
+    [name].hash
   end
 
-  def open!
-    opened << valve unless open?
-    self
-  end
-
-  def adjacent_paths
-    valve.tunnels.map do |neighbor|
-      ValvePath.new(neighbor, pressure, opened.clone, visited.clone)
-    end
-  end
-
-  def all_open?(valves)
-    (valves - opened).empty?
-  end
-
-  def to_s
-    "valve=#{valve.name} pressure=#{pressure} opened=#{opened.map(&:name).inspect} visited=#{visited.map(&:name).inspect}"
+  def can_open?
+    flow_rate.positive?
   end
 end
 
 class ValveGraph
-  attr_accessor :valves, :time_remaining
+  attr_accessor :valve_map, :distance, :next_node, :start
 
   def initialize(input)
-    self.valves = {}
-    self.time_remaining = 30
     parse_input(input)
+    self.start = valve_map['AA']
+
+    num_valves = valve_map.size
+    self.distance = Matrix.build(num_valves, num_valves) { Float::INFINITY }
+    self.next_node = Matrix.build(num_valves, num_valves) { nil }
+  end
+
+  def num_valves
+    @num_valves ||= valve_map.size
   end
 
   def release_pressure
-    max_path = ValvePath.new(valves['AA'], 0)
-    paths = [max_path]
+    compute_distances
 
-
-    until time_remaining.zero?
-      next_step = []
-
-      puts "=== Minute: #{31 - time_remaining} =="
-      puts
-
-      until paths.empty?
-        path = paths.shift
-        path.update_pressure
-        max_path = path if path.pressure > max_path.pressure
-
-        if path.all_open?(valves.values)
-          next_step << path
-        elsif path.open?
-          next_step += path.adjacent_paths
-        else
-          next_step << path.open!
-        end
-
-      end
-
-      puts "max_path: #{max_path}"
-      puts
-
-      paths = next_step
-      self.time_remaining -= 1
-    end
-
-    max_path
+    valve_map.each_value |
+    shortest_path = path(start, valve_map['CC'])
   end
 
   private
 
+  def compute_distances
+    # Floyd-Warshall to compute paths between each combination of vertices
+    valve_map.each_value do |valve|
+      valve.tunnels.each do |neighbor|
+        distance[valve.id, neighbor.id] = neighbor.can_open? ? 2 : 1
+        next_node[valve.id, neighbor.id] = neighbor
+      end
+
+      distance[valve.id, valve.id] = 0
+      next_node[valve.id, valve.id] = valve
+    end
+
+    num_valves.times do |k|
+      num_valves.times do |i|
+        num_valves.times do |j|
+          if distance[i, j] > distance[i, k] + distance[k, j]
+            distance[i, j] = distance[i, k] = distance[k, j]
+            next_node[i, j] = next_node[i, k]
+          end
+        end
+      end
+    end
+  end
+
+  def path(valve, other_valve)
+    return [] if next_node[valve.id, other_valve.id].nil?
+
+    path = [valve]
+
+    while valve.id != other_valve.id
+      valve = next_node[valve.id, other_valve.id]
+      path << valve
+    end
+
+    path
+  end
+
   def parse_input(lines)
+    self.valve_map = {}
+
     lines.each do |line|
       matches = line.match(
         /Valve (?<name>[A-Z]+) has flow rate=(?<flow_rate>[0-9]+); tunnels? leads? to valves? (?<tunnels>.*)/
@@ -120,7 +116,7 @@ class ValveGraph
   end
 
   def find_or_create_valve(name)
-    valves[name] ||= Valve.new(name)
+    valve_map[name] ||= Valve.new(name)
   end
 end
 
@@ -128,4 +124,6 @@ input = File.readlines('./test-input.txt').map(&:chomp)
 
 g = ValveGraph.new(input)
 
-puts "Released pressure: #{g.release_pressure}"
+shortest_path = g.release_pressure
+
+puts shortest_path
